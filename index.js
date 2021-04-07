e6d49b379562bf5dd6f9933700da8d7e426fabef
simonemayers <simone21295@gmail.com>
2021-04-06 22:55:02 -0400

    commit tree b3b29ad50740b6c03f23138b881defe7aa562bef

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+// function multiply(2, 30){
+    // return num1 * num2;
+// }
+// const multiply = 2 * 32; 
+
+// const multiply = function multiply(num1, num2) {
+    // return num1 * num2; 
+    // }
+
+const num1 = 2; 
+const num2 = 31; 
+const num3 = 9; 
+const num4 = 5; 
+// const set = [2, 20, 10, 9]
+
+
+
+const multiply = num1 * num2; 
+const random = Math.ceil(Math.random()); 
+const mod = num3 % num4; 
+const max = Math.max(2, 20, 10, 9);
+
+
+
+
